758a92d675e37fb6ca427d41225f9093af8f035b
404himel <mdtanvirmahmudhimel@gmail.com>
2025-08-13 22:53:43 +0600

    Updated

--- db_project/settings.py
+++ db_project/settings.py
@@ -78,7 +78,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '2025projecttemp@gmail.com'       
-EMAIL_HOST_PASSWORD = 'wufh qvad mqws swpr'
+
 
 
 ROOT_URLCONF = 'db_project.urls'
